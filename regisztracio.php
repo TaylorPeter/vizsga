b357abd82f561ecd8038957fbbd0a8cd8bfb07fb
Bricsi00 <73174708257@szily.hu>
2021-11-30 14:35:11 +0100

    regster

---
+++ regisztracio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROGw</title>
+    <link rel="stylesheet" href="VizsgaCSS.css">
+</head>
+<body>
+    
+    <div>   
+        <ul>
+            <li><a href="">Főoldal</a></li>
+            <li><a href="">Legfrissebb</a></li>
+            <li><a href="">Felkapott</a></li>
+            <li><a href="">Kategória</a></li>
+        <div class="legordulo" style="float:right;">
+        <button class="legordulogomb">Belépés</button>
+        <div class="legordulo-content">
+                <a href="Bejelentkezes.php">Bejelentkezés</a>
+                <a href="regisztracio.php">Regisztráció</a>
+        </div>
+        </div>
+
+            
+        </ul>
+    </div>
+
+
+    <script src="VizsgaJS.js"></script>
+</body>
+
+</html>
