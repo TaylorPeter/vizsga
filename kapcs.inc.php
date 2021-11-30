70525e3ceaa535728bc83249917f9e8512a51a9a
TalasiNorbert <talasi.norbert&gmail.com>
2021-11-30 14:13:51 +0100

    Kapcsolat

---
+++ kapcs.inc.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("localhost","root","","webshop");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  print( "Adatbázis kapcsolódási hiba!  " . mysqli_connect_error());
+  }
+
+
+
+ 
+ 
+ 
+ 
+  
+?>
