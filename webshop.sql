cac0505492695b4b8311b6b6cb47ed7ee630f98d
Bricsi00 <94435830+Bricsi00@users.noreply.github.com>
2021-12-07 14:19:19 +0100

    Add files via upload

---
+++ webshop.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2021. Dec 07. 14:18
+-- Kiszolgáló verziója: 10.4.14-MariaDB
+-- PHP verzió: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `webshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kosar`
+--
+
+CREATE TABLE `kosar` (
+  `kosarid` bigint(20) NOT NULL,
+  `termekid` bigint(20) NOT NULL,
+  `vevoid` bigint(20) NOT NULL,
+  `vasarlasid` bigint(20) NOT NULL,
+  `kosarosszeg` int(7) NOT NULL,
+  `db` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `termek`
+--
+
+CREATE TABLE `termek` (
+  `termekid` bigint(20) NOT NULL,
+  `nev` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `ar` int(6) NOT NULL,
+  `Kulcs` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `Mufaj` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
+  `lerias` varchar(400) COLLATE utf8_hungarian_ci NOT NULL,
+  `kep` varchar(100) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `vasarlas`
+--
+
+CREATE TABLE `vasarlas` (
+  `vasarlasid` bigint(20) NOT NULL,
+  `datum` date NOT NULL,
+  `osszeg` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `vevo`
+--
+
+CREATE TABLE `vevo` (
+  `vevoid` bigint(20) NOT NULL,
+  `nev` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  `jelszo` varchar(30) COLLATE utf8_hungarian_ci NOT NULL,
+  `egyenleg` int(10) NOT NULL,
+  `jatekok` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `kosar`
+--
+ALTER TABLE `kosar`
+  ADD PRIMARY KEY (`kosarid`);
+
+--
+-- A tábla indexei `termek`
+--
+ALTER TABLE `termek`
+  ADD PRIMARY KEY (`termekid`);
+
+--
+-- A tábla indexei `vasarlas`
+--
+ALTER TABLE `vasarlas`
+  ADD PRIMARY KEY (`vasarlasid`);
+
+--
+-- A tábla indexei `vevo`
+--
+ALTER TABLE `vevo`
+  ADD PRIMARY KEY (`vevoid`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `kosar`
+--
+ALTER TABLE `kosar`
+  MODIFY `kosarid` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `termek`
+--
+ALTER TABLE `termek`
+  MODIFY `termekid` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `vasarlas`
+--
+ALTER TABLE `vasarlas`
+  MODIFY `vasarlasid` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `vevo`
+--
+ALTER TABLE `vevo`
+  MODIFY `vevoid` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
